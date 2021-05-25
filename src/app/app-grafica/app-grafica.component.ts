6793198b5f4e5b1585ccdb522c2d224f3cb74e01
Matias <matiasshedden@gmail.com>
2021-05-24 22:43:15 -0300

    Finalizado app-grafica

--- src/app/app-grafica/app-grafica.component.ts
+++ src/app/app-grafica/app-grafica.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-grafica',
+  selector: 'app-grafica',
   templateUrl: './app-grafica.component.html',
   styleUrls: ['./app-grafica.component.scss']
 })
 export class AppGraficaComponent implements OnInit {
-
+  cuadernoA5 = {
+    "size" : "A5",
+    "price": 250,
+    "stock": 2
+  }
   constructor() { }
 
   ngOnInit(): void {
